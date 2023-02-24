589587b193e23c3370839a0c5c1592d24542d8d9
danu <danujagreru@gmail.com>
2023-02-25 00:54:09 +0530

    create a IO Database

---
+++ customer-manage/src/main/java/lk/ijse/customermanage/db/DataStore.java
@@ -0,0 +1,15 @@
+package lk.ijse.customermanage.db;
+
+/*
+    @author DanujaV
+    @created 2/25/23 - 12:51 AM   
+*/
+
+import lk.ijse.customermanage.dto.Customer;
+
+public class DataStore {
+    public static boolean writeObject(Customer customer) {
+
+        return false;
+    }
+}

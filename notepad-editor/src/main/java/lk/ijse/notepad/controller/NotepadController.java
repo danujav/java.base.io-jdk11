0dfb49f1f2f4b6da11d2e9c7c58696e46a77fa9a
danu <danujagreru@gmail.com>
2023-02-25 23:10:46 +0530

    complete notepad UI

--- notepad-editor/src/main/java/lk/ijse/notepad/controller/NotepadController.java
+++ notepad-editor/src/main/java/lk/ijse/notepad/controller/NotepadController.java
@@ -5,5 +5,16 @@ package lk.ijse.notepad.controller;
     @created 2/25/23 - 10:55 PM   
 */
 
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class NotepadController {
+    @FXML
+    private JFXTextArea txtArea;
+
+    @FXML
+    void itmSaveOnAction(ActionEvent event) {
+        String msg = txtArea.getText();
+    }
 }

406142ddf9facb7c261c02653fc7129f586c961d
danu <danujagreru@gmail.com>
2023-02-25 23:36:37 +0530

    notepad editor done

--- notepad-editor/src/main/java/lk/ijse/notepad/controller/NotepadController.java
+++ notepad-editor/src/main/java/lk/ijse/notepad/controller/NotepadController.java
@@ -9,12 +9,28 @@ import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.io.*;
+
 public class NotepadController {
     @FXML
     private JFXTextArea txtArea;
 
     @FXML
-    void itmSaveOnAction(ActionEvent event) {
+    void itmSaveOnAction(ActionEvent event) throws IOException {
         String msg = txtArea.getText();
+
+        File file = new File("/home/danu/Desktop/abc.txt");
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(msg);
+
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

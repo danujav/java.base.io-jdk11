4810ae341d89a52b237dafed5d188b978e63bf7d
danu <danujagreru@gmail.com>
2023-02-26 00:16:04 +0530

    file open and save done using Input & Output Stream

--- notepad-editor/src/main/java/lk/ijse/notepad/controller/NotepadController.java
+++ notepad-editor/src/main/java/lk/ijse/notepad/controller/NotepadController.java
@@ -8,20 +8,24 @@ package lk.ijse.notepad.controller;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class NotepadController {
     @FXML
     private JFXTextArea txtArea;
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private MenuBar menu;
 
     @FXML
     void itmSaveOnAction(ActionEvent event) {
         String msg = txtArea.getText();
-
-        File file = new File("/home/danu/Desktop/abc.txt");
-
         //through character stream
         /*try {
             if(!file.exists()) {
@@ -37,10 +41,9 @@ public class NotepadController {
         }*/
 
         //through byte stream
-
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("save a file");
-        File saveFile = fileChooser.showSaveDialog(txtArea.getScene().getWindow());
+        fileChooser.setTitle("Save a file");
+        File saveFile = fileChooser.showSaveDialog(root.getScene().getWindow());
 
         if(saveFile == null) return;
 
@@ -50,6 +53,28 @@ public class NotepadController {
             fos.write(bytes);
             fos.close();
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void itmOpenOnAction(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open a file");
+        File openFile = fileChooser.showOpenDialog(root.getScene().getWindow());
+
+        if(openFile == null) return;
+
+        try {
+            FileInputStream fis = new FileInputStream(openFile);
+            byte[] bytes = fis.readAllBytes();
+
+            String inputStr = new String(bytes);
+
+            txtArea.setText(inputStr);
+
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
